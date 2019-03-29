769a79b8c178459593dd790672fe154cacd49df0
John Vivian <jtvivian@gmail.com>
2019-03-29 08:58:14 -0700

    Add test skeleton

---
+++ tests/test_gene_outlier_detection.py
@@ -0,0 +1,38 @@
+def test_select_k_best_genes():
+    pass
+
+
+def test_get_sample():
+    pass
+
+
+def test_load_df():
+    pass
+
+
+def test_pca_distances():
+    pass
+
+
+def test_run_model():
+    pass
+
+
+def test_calculate_weights():
+    pass
+
+
+def test_plot_weights():
+    pass
+
+
+def test_posterior_predictive_check():
+    pass
+
+
+def test_posterior_predictive_pvals():
+    pass
+
+
+def test_pickle_model():
+    pass
