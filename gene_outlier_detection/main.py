44e2f56b8040653ccc353cca93d1a834822e97e8
John Vivian <jtvivian@gmail.com>
2019-04-15 11:10:05 -0700

    Improve CLI and help (resolves #17)
    - Add docstring for display_runtime

--- gene_outlier_detection/main.py
+++ gene_outlier_detection/main.py
@@ -181,62 +181,88 @@ def run(opts: Namespace, num_backgrounds: int):
 
 @click.command()
 @click.option(
-    "--sample", required=True, type=str, help="Sample(s) by Genes matrix (csv/tsv/hd5)"
+    "-s",
+    "--sample",
+    required=True,
+    type=str,
+    help="Sample(s) by Genes matrix (csv/tsv/hd5)",
 )
 @click.option(
+    "-b",
     "--background",
     required=True,
     type=str,
     help="Samples by Genes matrix with metadata columns first "
-    "(including a group column that discriminates samples by some category) (csv/tsv/hd5)",
-)
-@click.option("--name", required=True, type=str, help="Name of sample in sample matrix")
-@click.option(
-    "--gene-list", type=str, help="Single column file of genes to use for training"
+    "(including a categorical column that discriminates samples by some category) (csv/tsv/hd5)",
 )
 @click.option(
-    "--out-dir", default="./", type=str, show_default=True, help="Output directory"
+    "-n",
+    "--name",
+    required=True,
+    type=str,
+    help="Name of row in the sample matrix that corresponds to the desired sample to run",
 )
 @click.option(
+    "-l",
+    "--gene-list",
+    type=str,
+    help="Single column file of genes to train model and derive p-values for",
+)
+@click.option(
+    "-o",
+    "--out-dir",
+    default="./",
+    type=str,
+    show_default=True,
+    help="Output directory",
+)
+@click.option(
+    "-g",
     "--group",
     default="tissue",
     show_default=True,
     type=str,
-    help="Categorical column vector in the background matrix",
+    help="Name of the categorical column vector in the background matrix",
 )
 @click.option(
+    "-c",
     "--col-skip",
     default=1,
     show_default=True,
     type=int,
-    help="Number of metadata columns to skip in background matrix so remainder are genes",
+    help="Number of metadata columns to skip in background matrix. All columns after this value should be genes",
 )
 @click.option(
+    "-n",
     "--num-backgrounds",
     "n_bg",
     default=5,
     type=int,
     show_default=True,
-    help="Maximum number of background categorical groups to include in the model training. Model will run starting with one background dataset and iteratively add more until the p-values converge.",
+    help="Maximum number of background categorical groups to include in the model training. "
+    "Model will run starting with one background dataset and iteratively add more until the p-values converge.",
 )
 @click.option(
+    "-m",
     "--max-genes",
     default=125,
     type=int,
     show_default=True,
-    help="Maximum number of genes to run. I.e. if a gene list is input, how many additional"
-    "genes to add via SelectKBest. Useful for improving beta coefficients"
-    "if gene list does not contain enough tissue-specific genes.",
+    help="Maximum number of genes to run. I.e. if a gene list is provided, how many additional genes to add via "
+    "SelectKBest. Useful for improving beta coefficients if gene list does not contain enough tissue-specific "
+    "genes. A good rule of thumb is to set --max-genes to 1.5 times the number of genes in --gene-list",
 )
 @click.option(
+    "-t",
     "--num-training-genes",
     "n_train",
     default=50,
     type=int,
     show_default=True,
-    help="If gene-list is empty, will use SelectKBest to choose gene set.",
+    help="If gene-list is empty, will use SelectKBest to choose gene set. Not typically useful outside of testing.",
 )
 @click.option(
+    "-p",
     "--pval-convergence-cutoff",
     "pval_cutoff",
     default=0.99,
@@ -245,6 +271,7 @@ def run(opts: Namespace, num_backgrounds: int):
     help="P-value Pearson correlation cutoff to stop adding additional background datasets.",
 )
 @click.option(
+    "-d",
     "--disable-iter",
     is_flag=True,
     help="This flag disables iterative runs and runs one model with `--num-backgrounds`",
